458c121d280a19f42ae59e97b517a590b0fadf02
PairApp <79168736+PairApp@users.noreply.github.com>
2022-11-17 12:57:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
   return <h2>tabs project setup</h2>
